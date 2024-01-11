c8677d378cc6dbc8c40ad56039509f6358e55fe4
JavviFdeez <javifdez195@gmail.com>
2024-01-11 12:06:09 +0100

    Creacion de las clases User y SportActivity

---
+++ src/main/java/com/Model/SportActivity.java
@@ -0,0 +1,141 @@
+package com.Model;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "sport_activities")
+public class SportActivity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "distance")
+    private double distance;
+
+    @Column(name = "duration")
+    private int duration;
+
+    @Column(name = "elevation")
+    private int elevation;
+
+    @Column(name = "sport")
+    private String sport;
+
+    @Column(name = "activity_date")
+    private Date activityDate;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "activity_type")
+    private String activityType;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    // Constructor por defecto
+    public SportActivity() {}
+
+    // Constructor con parámetros
+    public SportActivity(double distance, int duration, int elevation, String sport, Date activityDate,
+                         String title, String description, String activityType, User user) {
+        this.distance = distance;
+        this.duration = duration;
+        this.elevation = elevation;
+        this.sport = sport;
+        this.activityDate = activityDate;
+        this.title = title;
+        this.description = description;
+        this.activityType = activityType;
+        this.user = user;
+    }
+
+    // Getters y setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public void setDistance(double distance) {
+        this.distance = distance;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public int getElevation() {
+        return elevation;
+    }
+
+    public void setElevation(int elevation) {
+        this.elevation = elevation;
+    }
+
+    public String getSport() {
+        return sport;
+    }
+
+    public void setSport(String sport) {
+        this.sport = sport;
+    }
+
+    public Date getActivityDate() {
+        return activityDate;
+    }
+
+    public void setActivityDate(Date activityDate) {
+        this.activityDate = activityDate;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getActivityType() {
+        return activityType;
+    }
+
+    public void setActivityType(String activityType) {
+        this.activityType = activityType;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    // Métodos adicionales según sea necesario
+}
---
+++ src/main/java/com/Model/User.java
@@ -0,0 +1,73 @@
+package com.Model;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "mail")
+    private String mail;
+
+    @Column(name = "password")
+    private String password;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<SportActivity> activities;
+
+    public User() {}
+
+    public User(String name, String mail, String password) {
+        this.name = name;
+        this.mail = mail;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<SportActivity> getActivities() {
+        return activities;
+    }
+
+    public void setActivities(List<SportActivity> activities) {
+        this.activities = activities;
+    }
+}
